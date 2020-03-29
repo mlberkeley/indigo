68ee2d94a043cdf514c2dad8922e9a56f261f60d
Brandon <brandon@btrabucco.com>
2020-03-29 03:17:47 -0700

    finished beam search

--- indigo/algorithms/beam_search.py
+++ indigo/algorithms/beam_search.py
@@ -29,12 +29,16 @@ def beam_search(model_features,
 
     sequence: tf.Tensor
         a tensor that contains output word ids that were taken
-        when decoding using the transformer"""
+        when decoding using the transformer
+    log_p: tf.Tensor
+        the log probability of predicted sentences under the
+        current transformer model"""
 
     batch_size = tf.shape(model_features.queries)[0]
     closed_beams = tf.fill([batch_size * beam_size], False)
     log_p = tf.zeros([batch_size * beam_size])
 
+    # tile the batch size of teh region feature by beam size
     region_features = RegionFeatureInput(
         features=tf.tile(
             model_features.values.features, [beam_size, 1, 1]),
@@ -43,6 +47,7 @@ def beam_search(model_features,
         detections=tf.tile(
             model_features.values.detections, [beam_size, 1]))
 
+    # tile the batch size of the model inputs by the beam size
     model_features = TransformerInput(
         queries=tf.fill([batch_size * beam_size, 1], 2),
         values=region_features,
@@ -50,38 +55,50 @@ def beam_search(model_features,
         values_mask=tf.tile(
             model_features.values_mask, [beam_size, 1]))
 
+    # loop for a maximum of max_iterations decoding steps
     for i in range(max_iterations):
 
+        # exit if all beams have finished being decoded
         if tf.reduce_all(closed_beams):
             break
 
+        # run the model for a single step
         logits = tf.math.log_softmax(model(model_features)[:, -1, :])
         values, ids = tf.math.top_k(logits, k=beam_size)
 
+        # calculate the log probability for every candidate sentence
         log_p = log_p[:, tf.newaxis] + tf.where(
             closed_beams[:, tf.newaxis], tf.zeros_like(values), values)
         log_p = tf.reshape(log_p, [batch_size, beam_size * beam_size])
         log_p, beam_ids = tf.math.top_k(log_p, k=beam_size)
         log_p = tf.reshape(log_p, [batch_size * beam_size])
 
+        # select the sentence with maximum log probability
         ids = tf.where(
             closed_beams[:, tf.newaxis], tf.zeros_like(ids), ids)
         ids = tf.reshape(ids, [batch_size, beam_size * beam_size])
         ids = tf.gather(ids, beam_ids, batch_dims=1)
         ids = tf.reshape(ids, [batch_size * beam_size])
 
+        # record if any of the beams have finished decoding
         closed_beams = tf.logical_or(
             closed_beams, tf.equal(ids, 3))
+
+        # concatenate the selected words with a partial sentence
         next_queries = tf.concat(
             [model_features.queries, ids[:, tf.newaxis]], 1)
+
+        # format the inputs for the transformer in the next round
         model_features = TransformerInput(
             queries=next_queries,
             values=model_features.values,
             queries_mask=tf.fill(tf.shape(next_queries), True),
             values_mask=model_features.values_mask)
 
+    # decoding is finished so un flatten the beam dimension
+    # returns a shape like [batch_size, beam_size, sequence_length]
     queries = tf.stack(tf.split(
         model_features.queries, beam_size, axis=0), axis=1)
-    log_p = tf.reshape(log_p, [batch_size, beam_size])
 
-    return queries, log_p
+    return queries, tf.reshape(
+        log_p, [batch_size, beam_size])
--- indigo/algorithms/greedy_search.py
+++ indigo/algorithms/greedy_search.py
@@ -24,36 +24,50 @@ def greedy_search(model_features,
 
     sequence: tf.Tensor
         a tensor that contains output word ids that were taken
-        when decoding using the transformer"""
+        when decoding using the transformer
+    log_p: tf.Tensor
+        the log probability of predicted sentences under the
+        current transformer model"""
 
     batch_size = tf.shape(model_features.queries)[0]
     closed_beams = tf.fill([batch_size], False)
     log_p = tf.zeros([batch_size])
 
+    # create an empty partial sentence and corresponding mask
     model_features = TransformerInput(
         queries=tf.fill([batch_size, 1], 2),
         values=model_features.values,
         queries_mask=tf.ones([batch_size, 1], tf.float32),
         values_mask=model_features.values_mask)
 
+    # loop for a maximum of max_iterations decoding steps
     for i in range(max_iterations):
 
+        # exit if all beams have finished being decoded
         if tf.reduce_all(closed_beams):
             break
 
+        # run the model for a single step
         logits = tf.math.log_softmax(model(model_features)[:, -1, :])
         values, ids = tf.math.top_k(logits, k=1)
 
+        # calculate the log probability for every candidate sentence
         log_p = log_p + tf.where(
             closed_beams, tf.zeros_like(values), values)
 
+        # prepare the decoded word ids
         ids = tf.squeeze(ids, 1)
         ids = tf.where(closed_beams, tf.zeros_like(ids), ids)
 
+        # record if any of the beams have finished decoding
         closed_beams = tf.logical_or(
             closed_beams, tf.equal(ids, 3))
+
+        # concatenate the selected words with a partial sentence
         next_queries = tf.concat(
             [model_features.queries, ids[:, tf.newaxis]], 1)
+
+        # format the inputs for the transformer in the next round
         model_features = TransformerInput(
             queries=next_queries,
             values=model_features.values,
