b8af48b9482ce40314a5918ac264d576d8214b7a
Brandon <brandon@btrabucco.com>
2020-03-31 01:32:14 -0700

    add merged pointer and logits for indigo model

--- indigo/nn/layers/pointer_and_logits.py
+++ indigo/nn/layers/pointer_and_logits.py
@@ -1,13 +1,14 @@
-from indigo.nn.base.block import Block
-from indigo.nn.ops.attention import causal_mask
+from indigo.nn.layers.pointer import Pointer
+from indigo.nn.base.logits import Logits
 import tensorflow as tf
 
 
-class Pointer(tf.keras.layers.Layer):
+class PointerAndLogits(tf.keras.layers.Layer):
 
     def __init__(self,
                  hidden_size,
                  output_size,
+                 logits_size,
                  causal=True,
                  logits_per_slot=2,
                  **kwargs):
@@ -22,25 +23,36 @@ class Pointer(tf.keras.layers.Layer):
         output_size: int
             the number of output units used by the network blocks
             used by this layer
+        logits_size: int
+            the number of units in the vector space of the logits
+            of a transformer model
         causal: bool
             specifies is the transformer should decoding using
             a causal mask to preserve the auto regressive property
         logits_per_slot: int
             specifies the number of logits per element the pointer
             network attends to; default is 2"""
-        super(Pointer, self).__init__()
+        super(PointerAndLogits, self).__init__()
 
+        self.logits = Logits(logits_size, **kwargs)
+        self.pointer = Pointer(hidden_size,
+                               output_size,
+                               causal=True,
+                               logits_per_slot=2,
+                               **kwargs)
+
+        # these parameters need to be stored so that
+        # tf.keras.model.save_model works
         self.hidden_size = hidden_size
         self.output_size = output_size
+        self.logits_size = logits_size
         self.causal = causal
         self.logits_per_slot = logits_per_slot
         self.kwargs = kwargs
-        self.block = Block(
-            hidden_size, output_size * (1 + logits_per_slot), **kwargs)
 
     def call(self, inputs, **kwargs):
-        """Runs a forward pass on a pointer network that generates
-        permutation matrices in log space
+        """Runs a forward pass on the logits of a transformer
+        inputs is an instance of TransformerInput
 
         Arguments:
 
@@ -50,31 +62,15 @@ class Pointer(tf.keras.layers.Layer):
 
         Returns:
 
-        outputs: tf.Tensor
-            a permutation matrix in log space that has the same shape
-            as the transformer attention weights"""
+        pointer_logits: tf.Tensor
+            the logits of a pointer network used to select locations to
+            insert words in a transformer
+        logits: tf.Tensor
+            the logits of a transformer model used for word prediction
+            or during a search algorithm"""
 
-        # map the sequence into a latent space
-        features = self.block(inputs.queries, **kwargs)
-        q = features[..., :self.output_size]
-
-        # reshape keys to have logits_per_slot more time steps
-        shape = tf.multiply(tf.shape(q), [1, self.logits_per_slot, 1])
-        k = tf.reshape(features[..., self.output_size:], shape)
-        scores = tf.matmul(q, k, transpose_b=True)
-
-        # prevent the permutation matrix from assigning mass to
-        # out of bounds elements
-        mask = tf.logical_and(tf.expand_dims(inputs.queries_mask, 2),
-                              tf.expand_dims(inputs.queries_mask, 1))
-        if self.causal:
-            mask = tf.logical_and(mask, causal_mask(scores))
-
-        # filter by removing logits for elements that are invalid
-        # mask must be repeated to correct the shape
-        mask = tf.repeat(mask, [1, 1, self.logits_per_slot])
-        return tf.where(
-            mask, scores, tf.fill(tf.shape(scores), -999999.))
+        return self.pointer(
+            inputs, **kwargs), self.logits(inputs, **kwargs)
 
     def get_config(self):
         """Creates a state dictionary that can be used to rebuild
@@ -89,10 +85,11 @@ class Pointer(tf.keras.layers.Layer):
         # these are all that is needed to rebuild this class
         config = dict(hidden_size=self.hidden_size,
                       output_size=self.output_size,
+                      logits_size=self.logits_size,
                       causal=self.causal,
                       logits_per_slot=self.logits_per_slot,
                       **self.kwargs)
 
-        base_config = super(Block, self).get_config()
+        base_config = super(PointerAndLogits, self).get_config()
         return dict(list(base_config.items()) +
                     list(config.items()))
